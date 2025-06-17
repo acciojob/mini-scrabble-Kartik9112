218722c24082c63100e03e1c3d66802c03b6adf1
kartik sharma <118871654+Kartik9112@users.noreply.github.com>
2025-06-17 13:52:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const textarea = document.getElementById('evaluatedText');
+const countDisplay = document.getElementById('letterCount');
+
+textarea.addEventListener('input', () => {
+  const textLength = textarea.value.length;
+  countDisplay.textContent = textLength;
+});
